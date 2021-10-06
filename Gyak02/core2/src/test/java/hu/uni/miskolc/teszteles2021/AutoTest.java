0bbd3dc69ba2a444191b3c030bf8fb6993369a8d
Márton Bán <marton.bn@gmail.com>
2021-10-06 08:43:34 +0200

    H5 eleje

---
+++ Gyak02/core2/src/test/java/hu/uni/miskolc/teszteles2021/AutoTest.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.teszteles2021;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class AutoTest extends TestCase {
+
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+
+}
